3fc22b10e0dfd20f928bf1f1d188226fd9eb6ec9
NOKECHINA\yanggang <yanggangc@163.com>
2022-08-05 00:14:03 +0800

    修复部分问题

---
+++ OpsLab/src/Ops.Host.Common/Utils/PageHelper.cs
@@ -0,0 +1,26 @@
+﻿namespace Ops.Host.Common.Utils;
+
+public static class PageHelper
+{
+    /// <summary>
+    /// 计算页数
+    /// </summary>
+    /// <param name="count">总数量</param>
+    /// <param name="pageSize">每页数量</param>
+    /// <returns></returns>
+    /// <exception cref="System.DivideByZeroException"></exception>
+    public static long GetPageCount(long count, int pageSize)
+    {
+        if (count == 0)
+        {
+            return 0;
+        }
+
+        if (count <= pageSize)
+        {
+            return 1;
+        }
+
+        return count / pageSize + (count % pageSize > 0 ? 1 : 0);
+    }
+}
--- OpsLab/src/Ops.Host.Core/Extensions/FreeSqlExtensions.cs
+++ OpsLab/src/Ops.Host.Core/Extensions/FreeSqlExtensions.cs
@@ -4,6 +4,13 @@ internal static class FreeSqlExtensions
 {
     public static void Mapper(this IFreeSql freeSql)
     {
-        
+        // 枚举类型添加注解 [FreeSql.DataAnnotations.Column(MapType = typeof(int))]
+        freeSql.Aop.ConfigEntityProperty += (s, e) =>
+        {
+            if (e.Property.PropertyType.IsEnum)
+            {
+                e.ModifyResult.MapType = typeof(int?);
+            }
+        };
     }
 }
